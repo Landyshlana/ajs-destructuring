b295c7d505a2cd5f92b35c32816cf06a28c814e0
Landyshlana <135726642+Landyshlana@users.noreply.github.com>
2024-04-07 15:26:12 +0300

    commit1

---
+++ src/js/__tests__/test.app.js
@@ -0,0 +1,37 @@
+import getSpecialAttack from '../app';
+const character = {
+	name: 'Лучник',
+	type: 'Bowman',
+	health: 50,
+	level: 3,
+	attack: 40,
+	defence: 10,
+	special: [{
+			id: 8,
+			name: 'Двойной выстрел',
+			icon: 'http://...',
+			description: 'Двойной выстрел наносит двойной урон'
+		},
+		{
+			id: 9,
+			name: 'Нокаутирующий удар',
+			icon: 'http://...'
+		},
+	]
+}
+test('тестирование функции getSpecialAttack', () => {
+	const expected = [{
+			id: 8,
+			name: 'Двойной выстрел',
+			icon: 'http://...',
+			description: 'Двойной выстрел наносит двойной урон'
+		},
+		{
+			id: 9,
+			name: 'Нокаутирующий удар',
+			icon: 'http://...',
+			description:'Описание недоступно'
+		},
+	]
+	expect(getSpecialAttack(character)).toEqual(expected);
+})
---
+++ src/js/app.js
@@ -0,0 +1,21 @@
+export default function getSpecialAttack(character) {
+
+	const result = [];
+
+	character.special.forEach((item) => {
+		const {
+			name,
+			id,
+			icon,
+			description = 'Описание недоступно',
+		} = item;
+		result.push({
+			name,
+			id,
+			icon,
+			description,
+		});
+	});
+
+	return (result);
+}
